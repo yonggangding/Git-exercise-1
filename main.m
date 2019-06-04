677e7e963f6ae7a9354466bcf676fd152a89500b
yonggang ding <strongfe@163.com>
2019-06-04 17:40:12 +0800

    calculate stiffness of new shape
    calculate stiffness of new shape based on stiffness pof original shape

--- main.m
+++ main.m
@@ -12,3 +12,14 @@ h1=9;
 m1=6;
 
 [Ixx1,Iyy1]=testFunc(b1,h1);
+
+% stiffness of original shape
+EIFlap=3.89e10;
+EIEfge=4.13e10;
+
+%derived the stiffness of new shape
+EIFlap1=EIFlap*Iyy/Iyy1;
+EIEdge1=EIEdge*Ixx/Ixx1;
+
+
+
